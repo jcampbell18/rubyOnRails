e48091cc887a3528164d90f5cac1e2ce394db058
Jason Campbell <jcryuu@gmail.com>
2020-11-25 15:39:12 -0800

    update readme, finished challenge 5

--- ruby/3_EssentialTraining/chapter5challenge.rb
+++ ruby/3_EssentialTraining/chapter5challenge.rb
@@ -16,10 +16,24 @@ require 'erb'
 
 APP_ROOT = File.expand_path(File.dirname(__FILE__))
 
-filepath = File.join(APP_ROOT, 'letters', 'us_presidents.csv')
-data = []
+csv = File.join(APP_ROOT, 'letters', 'us_presidents.csv')
+letter = File.join(APP_ROOT, 'letters', 'letter_template.txt')
+template = File.read(letter)
 
-CSV.foreach(filepath, headers: true) do |row|
-  data << row
+CSV.foreach(csv, headers: true) do |item|
+  @first_name = item["First Name"]
+  @last_name = item["Last Name"]
+  @title = "The History of " + item["State"]
+  @due_date = item["End"].nil? ? Time.now.year : item["End"]
+  @fee = "$ #{((Time.now.year - item["End"].to_i) * 1.00)}"
+
+  output = ERB.new(template).result(binding)
+
+  new_file = "letter_"
+  num = item["Number"].to_i < 10 ? "0#{item["Number"]}" : item["Number"]
+  new_file << "#{num}.txt"
+  new_filepath = File.join(APP_ROOT, 'letters', new_file)
+  File.write(new_filepath, output)
+
+  puts output
 end
-
